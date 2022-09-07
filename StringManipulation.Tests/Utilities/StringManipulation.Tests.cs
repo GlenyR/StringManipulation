7097270955eea2435d49d36d2108a72cd6533a94
Gleny Rodriguez <grodriguez@fulldevs.com>
2022-09-07 00:37:08 -0600

    add the SortingOperations method and unit tests.

---
+++ StringManipulation.Tests/Utilities/StringManipulation.Tests.cs
@@ -0,0 +1,101 @@
+﻿namespace StringManipulation.Tests.Utilities
+{
+    public class StringManipulationTests
+    {
+
+        [Theory]
+        [InlineData(new string[] {
+            "abaccadcc", 
+            "xyzxy",
+        }, new string[] { 
+            "ccccaaabd", 
+            "xxyyz", })]
+        [InlineData(new string[] {
+            "dulgvgzwqg",
+            "gxtjtmtywr",
+            "hnlnxiupgt",
+            "gzjotckivp",
+            "dpwwsdptae",
+            "pcscpilknb",
+            "btvyhhmflf",
+            "artrtnqxcr",
+            "nrtcmcoadn",
+            "fkdsgnekft"
+        }, new string[] {
+            "gggdlquvwz",
+            "tttgjmrwxy",
+            "nnghilptux",
+            "cgijkoptvz",
+            "ddppwwaest",
+            "ccppbiklns",
+            "ffhhblmtvy",
+            "rrrttacnqx",
+            "ccnnadmort",
+            "ffkkdegnst", })]
+        [InlineData(new string[] {
+            "wzenwebuau",
+            "vokfxzynwl",
+            "neldfeyrxk",
+            "wqadfiodgs",
+            "ykiuvzfcbc",
+        }, new string[] {
+            "eeuuwwabnz",
+            "fklnovwxyz",
+            "eedfklnrxy",
+            "ddafgioqsw",
+            "ccbfikuvyz", })]
+        [InlineData(new string[] {
+            "qakmc",
+            "rrtbk",
+            "vaixn",
+            "wmpnj",
+            "uproi",
+            "btska",
+            "ejqwr",
+            "elwlg",
+            "oaoiy",
+            "hrqkn",
+        }, new string[] {
+            "ackmq",
+            "rrbkt",
+            "ainvx",
+            "jmnpw",
+            "iopru",
+            "abkst",
+            "ejqrw",
+            "llegw",
+            "ooaiy",
+            "hknqr", })]
+        [InlineData(new string[] {
+            "pzjim",
+            "njnfq",
+            "xyohs",
+        }, new string[] {
+            "ijmpz",
+            "nnfjq",
+            "hosxy",})]
+        [InlineData(new string[] {
+           "xqycs",
+            "beoax",
+            "afkso",
+            "bldit",
+            "gwrys",
+        }, new string[] {
+            "cqsxy",
+            "abeox",
+            "afkos",
+            "bdilt",
+            "grswy", })]
+        public void Input_SortingOperations_ReturnsSortedStrings(string[] strs, string[] expected)
+        {
+            // Arrange
+            var stringManipulation = new StringManipulation.Utilities.StringManipulation();
+
+            // Act
+            var actual = stringManipulation.SortingOperations(strs);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ StringManipulation/Utilities/StringManipulation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation.Utilities
+{
+    public class StringManipulation
+    {
+
+        public string[] SortingOperations(string[] strs)
+        {
+            string[] sortedStrs = new string[strs.Length];
+            for (int strIdx = 0; strIdx < strs.Length; strIdx++)
+            {
+                var alphabetFrecuency = GetAlphabetCount(strs[strIdx]);
+                sortedStrs[strIdx] = string.Join("", alphabetFrecuency.OrderByDescending(o => o.Item2).Select(s => new string(s.Item1, s.Item2)).ToArray());
+            }
+            return sortedStrs;
+        }
+
+        private List<Tuple<char, int>>  GetAlphabetCount(string str)
+        {
+            var alphabetFrecuency = new List<Tuple<char, int>>();
+            for (int i = 0; i < 26; i++)
+            {
+                char character = (char)(i + 'a');
+                char[] charArray = str.ToCharArray();
+                var count = 0;
+                for (int p = 0; p < charArray.Length; p++)
+                {
+
+                    if (charArray[p] == character)
+                    {
+                        count++;
+                    }
+                }
+                
+                if (count > 0)
+                {
+                    alphabetFrecuency.Add(Tuple.Create(character, count));
+                }
+            }
+            return alphabetFrecuency;
+        }
+
+    }
+}
